2b182485cf4d225553caac3fdcf16209439e1eb0
Joseph Noblett <noblettjoe@gmail.com>
2024-04-08 23:09:03 -0500

    learing ts

---
+++ typescript/src/tutorial.ts
@@ -0,0 +1,31 @@
+
+let awesomeName:string = 'shakeAndBake';
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName); 
+
+// awesomeName = 50;
+
+let amount:number = 50;
+amount = 12
+
+let isAwesome = true;
+isAwesome = false;
+
+// section challenge 
+let myName:string = "   Joe     ";
+myName = myName.trim();
+
+let score:number = 100;
+score--
+
+let isOver21:boolean = true;
+isOver21 = false
+
+// Union Types
+
+let tax:number | string = 10;
+tax = 100;
+tax = '$1.00';
+
+let requestStatus: 'pending' | 'success' | 'error' = 'pending';
+
